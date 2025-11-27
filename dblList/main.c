6e3790055a8fd21ddea878aae98762fbe0796ae6
gounane <gounane.said@gmail.com>
2025-11-27 10:00:44 +0100

    dbllist

--- dblList/main.c
+++ dblList/main.c
@@ -1,21 +1,10 @@
 #include<stdio.h>
-#include<stdlib.h>
-typedef struct Node {
-    int data;
-    struct Node* next;
-    struct Node* prev;
-} Node;
-typedef Node * DblList;
-Node * getNode(int v){
-} 
-void printDblList(DblList head){
-}
-void insert(DblList head){
-}
+#include "tools.h"
 int main(){
     DblList head = NULL;
-    insert(5);
-    insert(7);
+
+    insert(head,getNode(5));
+    insert(head,getNode(7));
     printDblList(head);
 }
 
---
+++ dblList/tools.h
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct Node {
+    int data;
+    struct Node* next;
+    struct Node* prev;
+} Node;
+typedef Node * DblList;
+Node * getNode(int v){
+    Node * newNode=(Node *)malloc(sizeof(Node));
+    newNode->data=v;
+    newNode->next=NULL;
+    newNode->prev=NULL;
+    return newNode;
+} 
+void printDblList(DblList head){
+    Node *temp=head;
+    while(temp!=NULL) {
+        printf("%d", temp->data);
+        temp=temp->next;
+    }
+}
+void insert(DblList head, Node *newNode){
+    if(head==NULL) head=newNode;
+    else{
+        newNode->next=head;
+        head->prev=newNode;
+        head=newNode;
+    }
+}
