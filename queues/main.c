28fdd81b8e22cd7c6f4cf165d9aafca89f505cd1
gounane <gounane.said@gmail.com>
2025-12-04 09:31:53 +0100

    Queue

--- queues/main.c
+++ queues/main.c
@@ -3,19 +3,22 @@
 #define MAX 10
 
 typedef struct Queue{
-    int data;
+    int data[MAX];
     int tete,queue; 
 }Queue;
 
 void initQueue(Queue *f){
     f->tete=f->queue=-1;
 }
- printQueue(Queue f){
-
+void printQueue(Queue f){
+    for(int i=f.tete;i<f.queue+1;i++) printf("%d<-", f.data[i]);
+    printf("\n");
 
  }
 void enqueue(Queue *f, int data){
-
+    if(f->tete==-1) f->tete=0;
+    f->queue++;
+    f->data[f->queue]=data;
 }
 
 void dequeue(Queue *f){
@@ -29,7 +32,7 @@ int main(){
     enqueue(&f,4);
     enqueue(&f,9);
     enqueue(&f,3);
-    enqueue(&f,1);
+    //enqueue(&f,1);
     printQueue(f);
     return 0;
 }
